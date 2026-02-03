4f9559549cfae184497c5177d74aa2a0b060d34c
kimHyeonJu <kimhyesom1202@gmail.com>
2026-02-03 23:13:20 +0900

    [Feature] 기본 레이아웃 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Mandalart from '@components/Mandalart';
+import SideSection from '@components/SideSection';
+
 function App() {
   return (
-    <h1 className="font-bold text-white bg-black">Hello, Bloom!</h1>
-  )
+    <div className="flex">
+      <aside className="w-125 min-h-screen bg-primary">
+        <SideSection />
+      </aside>
+
+      <main className="w-full min-h-screen grow bg-gray-50 flex items-center justify-center">
+        <Mandalart />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
