542e6f82e29db5ff62b19adb56988c24c102ecf0
kimHyeonJu <kimhyesom1202@gmail.com>
2026-02-06 17:39:17 +0900

    [Modify] SideSection 제작
    - width, height가 초과되는 현상 수정

--- src/components/SideSection.tsx
+++ src/components/SideSection.tsx
@@ -3,12 +3,12 @@ import HorseImage from '@assets/images/horse.webp';
 
 export default function SideSection() {
   return (
-    <div className="flex flex-col justify-between lg:flex-col items-center min-w-full min-h-screen px-6 py-8 md:px-12 md:py-16 lg:px-6 lg:py-8">
+    <div className="flex flex-col justify-between items-center lg:flex-col min-h-screen px-6 py-8 md:px-12 md:py-16 lg:px-6 lg:py-8">
       <h1 className="text-9xl font-bold text-white">2026</h1>
       <img
         src={HorseImage}
         alt=""
-        className="w-full h-full md:w-150 md:h-150 lg:w-full object-contain"
+        className="block w-full h-full md:w-150 md:h-150 lg:w-full lg:h-full object-contain"
       />
       <Resolution />
     </div>
