a79847a4b4739010237a9a985e5a0e991be9e4d3
kimHyeonJu <kimhyesom1202@gmail.com>
2026-02-06 17:03:08 +0900

    [Feature] Resolution Section 제작
    - 올해의 다짐을 저장할 수 있는 localStorage 추가

--- src/components/Resolution.tsx
+++ src/components/Resolution.tsx
@@ -1,4 +1,25 @@
+import { useState, useEffect } from 'react';
+
 export default function Resolution() {
+  const [savedResolution, setSavedResolution] = useState(() => {
+    return localStorage.getItem('resolution') || '';
+  });
+
+  const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setSavedResolution(e.target.value);
+  };
+
+  // textarea 수정될 때 마다 localStorage 동기화
+  useEffect(() => {
+    const value = savedResolution;
+
+    if (value === '') {
+      localStorage.removeItem('resolution');
+    } else {
+      localStorage.setItem('resolution', value);
+    }
+  }, [savedResolution]);
+
   return (
     <div className="flex flex-col grow-0 justify-start items-start gap-3.5 w-full border-none bg-white rounded-2xl p-6">
       <h1 className="w-full text-[32px] md:text-[60px] lg:text-[40px] text-primary font-bold text-center lg:text-left">올해의 다짐</h1>
@@ -6,6 +27,8 @@ export default function Resolution() {
       <textarea
         className="resize-none w-full h-30 border-none hover:outline hover:outline-primary focus:outline-primary"
         placeholder="올해의 다짐을 작성해보세요."
+        onChange={handleTextareaChange}
+        value={savedResolution}
       />
     </div>
   )
