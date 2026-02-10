7767ed3e62a9e657a7e9926705f90c6e25f10395
kimHyeonJu <kimhyesom1202@gmail.com>
2026-02-10 20:50:22 +0900

    [Refactor] Home 파일 생성 및 App에 적용

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import Mandalart from '@components/Mandalart';
+import SideSection from '@components/SideSection';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col lg:flex-row min-w-screen">
+      <aside className="w-full lg:w-1/4 min-h-screen bg-primary">
+        <SideSection />
+      </aside>
+
+      <main className="w-full min-h-screen grow bg-gray-50 flex items-center justify-center">
+        <Mandalart />
+      </main>
+    </div>
+  );
+}
