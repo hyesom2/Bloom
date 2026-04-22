d6641a2fba88375546370d9d5139d0b708a85a34
kimHyeonJu <kimhyesom1202@gmail.com>
2026-04-22 22:11:18 +0900

    [Feature] 메뉴 아이콘 추가

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,6 @@
 import { useColorStore } from '@/store/useColorStore';
 import type { MenuProps } from '@/types/mandalart';
+import { FileDown, ImageDown, Palette } from 'lucide-react';
 import { useState } from 'react';
 import { HexColorPicker } from 'react-colorful';
 
@@ -8,14 +9,16 @@ export default function Menu({ exportPNG, exportJPG, exportPDF }: MenuProps) {
   const [showColorPicker, setShowColorPicker] = useState(false);
 
   return (
-    <ul className="absolute top-full left-0 w-50 flex flex-col justify-start items-start gap-2 bg-white p-2 rounded-lg">
-      <button
-        type="button"
+    <ul className="absolute top-full left-0 w-55 flex flex-col justify-start items-start gap-2 bg-white p-2 rounded-lg">
+      <li
         className="flex justify-start items-center w-full text-md text-black font-bold p-2 cursor-pointer hover:bg-gray-50 transition"
         onClick={() => setShowColorPicker(!showColorPicker)}
       >
-        색상 변경
-      </button>
+        <span className="flex justify-start items-center gap-1">
+          <Palette />
+          색상 변경
+        </span>
+      </li>
       {showColorPicker && (
         <div className="absolute top-0 left-full ml-2 sz-9999">
           <HexColorPicker color={color} onChange={setColor} />
@@ -25,19 +28,28 @@ export default function Menu({ exportPNG, exportJPG, exportPDF }: MenuProps) {
         className="flex justify-start w-full items-center text-md text-black font-bold p-2 cursor-pointer hover:bg-gray-50 transition"
         onClick={exportJPG}
       >
-        JPG 이미지로 저장
+        <span className="flex justify-start items-center gap-1">
+          <ImageDown />
+          JPG 이미지로 저장
+        </span>
       </li>
       <li
         className="flex justify-start w-full items-center text-md text-black font-bold p-2 cursor-pointer hover:bg-gray-50 transition"
         onClick={exportPNG}
       >
-        PNG 이미지로 저장
+        <span className="flex justify-start items-center gap-1">
+          <ImageDown />
+          PNG 이미지로 저장
+        </span>
       </li>
       <li
         className="flex justify-start w-full items-center text-md text-black font-bold p-2 cursor-pointer hover:bg-gray-50 transition"
         onClick={exportPDF}
       >
-        PDF 문서로 저장
+        <span className="flex justify-start items-center gap-1">
+          <FileDown />
+          PDF 문서로 저장
+        </span>
       </li>
     </ul>
   );
